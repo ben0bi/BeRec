de368c708a93f0dc01c4ad57978834278cee128d
benobi <ben0bi@mobi.le>
2020-02-10 06:41:06 +0100

    Externalized some values.

--- BURP.py
+++ BURP.py
@@ -12,89 +12,70 @@ import globals
 
 from time import sleep
 
-BTN_PLAYPAUSE = 14
-BTN_STOP = 15
-BTN_FWD = 18
-BTN_REW = 17
-
-BTN_VOLUP = 21
-BTN_VOLDOWN = 20
-BTN_REC = 16
-BTN_ANYTHING = 26
-
 # the actual position of the endless tape
 BURPos = 0
 
-# state of this player
-BURP_STATE = globals.BURPSTATE_PAUSE
-
 def BURP_Init():
 	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(BTN_PLAYPAUSE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(BTN_STOP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(BTN_FWD, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(BTN_REW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(BTN_REC, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(BTN_VOLUP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(BTN_VOLDOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(BTN_ANYTHING, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(globals.BTN_PLAYPAUSE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(globals.BTN_STOP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(globals.BTN_FWD, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(globals.BTN_REW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(globals.BTN_REC, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(globals.BTN_VOLUP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(globals.BTN_VOLDOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(globals.BTN_ANYTHING, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	return
 
-recpressed = 0
-playpausepressed = 0
 def BURP_UPDATE():
-	global recpressed
-	global playpausepressed
-	global BURP_STATE
-
-	pp=GPIO.input(BTN_PLAYPAUSE)
-	fw=GPIO.input(BTN_FWD)
-	rw=GPIO.input(BTN_REW)
-	st=GPIO.input(BTN_STOP)
-	rc=GPIO.input(BTN_REC)
-	vu=GPIO.input(BTN_VOLUP)
-	vd=GPIO.input(BTN_VOLDOWN)
-	an=GPIO.input(BTN_ANYTHING)
+	pp=GPIO.input(globals.BTN_PLAYPAUSE)
+	fw=GPIO.input(globals.BTN_FWD)
+	rw=GPIO.input(globals.BTN_REW)
+	st=GPIO.input(globals.BTN_STOP)
+	rc=GPIO.input(globals.BTN_REC)
+	vu=GPIO.input(globals.BTN_VOLUP)
+	vd=GPIO.input(globals.BTN_VOLDOWN)
+	an=GPIO.input(globals.BTN_ANYTHING)
 	print(pp,fw,rw,st,rc,vu,vd,an)
 
 # check if record was pressed.
-	if(rc==0 and recpressed==0):
-		recpressed = 1
-		if(BURP_STATE!=globals.BURPSTATE_REC and BURP_STATE!=globals.BURPSTATE_RECPAUSE):
-			if(BURP_STATE==globals.BURPSTATE_PLAY):
+	if(rc==0 and globals.PRESS_REC==0):
+		globals.PRESS_REC = 1
+		if(globals.BURP_STATE!=globals.BURPSTATE_REC and globals.BURP_STATE!=globals.BURPSTATE_RECPAUSE):
+			if(globals.BURP_STATE==globals.BURPSTATE_PLAY):
 				# TODO: stop the playing
 				print("play stopped")
-			BURP_STATE = globals.BURPSTATE_REC
+			globals.BURP_STATE = globals.BURPSTATE_REC
 			print "RECORD"
 		else:
 			# TODO: stop and save record
-			BURP_STATE = globals.BURPSTATE_PAUSE
+			globals.BURP_STATE = globals.BURPSTATE_PAUSE
 			print "STOP RECORD"
 	if(rc==1):
-		recpressed = 0
+		globals.PRESS_REC = 0
 	sleep(0.25)
 
 # check if playpause was pressed.
-	if(pp==0 and playpausepressed==0):
-		playpausepressed = 1
+	if(pp==0 and globals.PRESS_PP==0):
+		globals.PRESS_PP = 1
 		# pause record
-		if(BURP_STATE==globals.BURPSTATE_REC):
+		if(globals.BURP_STATE==globals.BURPSTATE_REC):
 			print("PAUSE RECORD")
-			BURP_STATE = globals.BURPSTATE_RECPAUSE
+			globals.BURP_STATE = globals.BURPSTATE_RECPAUSE
 		# pause play
-		elif(BURP_STATE==globals.BURPSTATE_PLAY):
+		elif(globals.BURP_STATE==globals.BURPSTATE_PLAY):
 			print("PAUSE PLAY")
-			BURP_STATE = globals.BURPSTATE_PAUSE
+			globals.BURP_STATE = globals.BURPSTATE_PAUSE
 		# continue record
-		elif(BURP_STATE==globals.BURPSTATE_RECPAUSE):
+		elif(globals.BURP_STATE==globals.BURPSTATE_RECPAUSE):
 			print("CONTINUE RECORD")
-			BURP_STATE=globals.BURPSTATE_REC
+			globals.BURP_STATE=globals.BURPSTATE_REC
 		# continue play
-		elif(BURP_STATE!=globals.BURPSTATE_RECPAUSE and BURP_STATE!=globals.BURPSTATE_PLAY):
+		elif(globals.BURP_STATE!=globals.BURPSTATE_RECPAUSE and globals.BURP_STATE!=globals.BURPSTATE_PLAY):
 			print("START/CONTINUE PLAY")
-			BURP_STATE=globals.BURPSTATE_PLAY
+			globals.BURP_STATE=globals.BURPSTATE_PLAY
 	if(pp==1):
-		playpausepressed = 0
+        globals.PRESS_PP = 0
 
 BURP_Init()
 
@@ -108,5 +89,3 @@ except KeyboardInterrupt:
 finally:
 	GPIO.cleanup()
 	print("done")
-
-
--- globals.py
+++ globals.py
@@ -1,3 +1,19 @@
+# Button BCM Values.
+# BURP buttons: The standard tape buttons.
+BTN_PLAYPAUSE = BTN_PP = 14
+BTN_STOP = 15
+BTN_FWD = 18
+BTN_REW = 17
+
+BTN_VOLUP = 21
+BTN_VOLDOWN = 20
+BTN_REC = 16
+BTN_ANYTHING = 26 # there is an 8th button on my pads.
+
+# The button-is-pressed flags.
+PRESS_PP = 0
+PRESS_REC = 0
+
 # global values for BURP
 
 # BURP states.
@@ -5,6 +21,18 @@
 # Endless Tape ET is one single file where all the stuff will be recorded to.
 # ET only has this one file and does not recognize other (MP3-)Files.
 # In the menu you may split ET in several files and such.
+
+# File Tape FT does record to several files, one for each recording.
+# FT also is the MP3 Player which reads full files from the SD-cards.
+# FT mode does not recognize the ET file and 
+# ET mode ONLY recognizes the ET file.
+BURPMODE_ET = 0
+BURPMODE_FT = 1
+
+BURP_MODE = BURPMODE_ET
+
+# The several states of the player.
+# If there is a REC state, you cannot FWD and REW.
 BURPSTATE_PAUSE = 0 # default state is not playing
 BURPSTATE_PLAY = 1 # playing
 BURPSTATE_REC = 2 # recording
@@ -12,5 +40,5 @@ BURPSTATE_RECPAUSE = 3 # pause record
 BURPSTATE_FWD = 4 # fast forward
 BURPSTATE_REW = 5 # fast backward
 
-# File Tape FT does record to several files, one for each recording.
-# FT also is the MP3 Player which reads full files from the SD-cards.
+# the actual burpstate
+BURP_STATE = BURPSTATE_PAUSE
